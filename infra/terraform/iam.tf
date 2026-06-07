b83d55494b9e56ff9e0d1bf8a4fc08004bad9732
daigo424 <ishii.daigo@gmail.com>
2026-06-07 22:49:45 +0900

    fix: way of referring to GitHub provider

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -1,11 +1,5 @@
-data "tls_certificate" "github" {
+data "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
-}
-
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
 }
 
 data "aws_iam_policy_document" "github_actions_assume_role" {
@@ -15,7 +9,7 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
 
     principals {
       type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.github.arn]
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
     }
 
     condition {
