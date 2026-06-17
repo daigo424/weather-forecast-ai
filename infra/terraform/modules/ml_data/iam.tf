efa28329995a1fb25e32530fc68d85abf48f13fb
daigo424 <ishii.daigo@gmail.com>
2026-06-17 22:06:10 +0900

    feat: integrate ml_data into module

---
+++ infra/terraform/modules/ml_data/iam.tf
@@ -0,0 +1,62 @@
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.github_repo}:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions_role" {
+  name               = "${var.name_prefix}-github-actions-role"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+data "aws_iam_policy_document" "github_actions_role_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+    resources = [
+      aws_s3_bucket.ml_data.arn,
+      "${aws_s3_bucket.ml_data.arn}/*",
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey",
+    ]
+    resources = [aws_kms_key.ml_data_key.arn]
+  }
+}
+
+resource "aws_iam_role_policy" "github_actions_role_policy" {
+  name   = "GithubActionsRolePolicy"
+  role   = aws_iam_role.github_actions_role.id
+  policy = data.aws_iam_policy_document.github_actions_role_policy.json
+}
