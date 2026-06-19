eb4c8d9d20b5a449bd3adcd81ffca680bfe83926
daigo424 <ishii.daigo@gmail.com>
2026-06-19 23:37:04 +0900

    feat: run helm with cli

--- infra/terraform/locals.tf
+++ infra/terraform/locals.tf
@@ -4,7 +4,5 @@ locals {
   is_test        = var.environment == "test"
   create_compute = local.is_test && var.compute_enabled
 
-  eks_cluster_name     = try(module.eks[0].cluster_name, "")
-  eks_cluster_endpoint = try(module.eks[0].cluster_endpoint, "")
-  eks_cluster_ca_cert  = local.create_compute ? base64decode(module.eks[0].cluster_certificate_authority_data) : ""
+  eks_cluster_name = try(module.eks[0].cluster_name, "")
 }
--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -9,15 +9,3 @@ provider "aws" {
     }
   }
 }
-
-provider "helm" {
-  kubernetes {
-    host                   = local.eks_cluster_endpoint
-    cluster_ca_certificate = local.eks_cluster_ca_cert
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", local.eks_cluster_name]
-    }
-  }
-}
