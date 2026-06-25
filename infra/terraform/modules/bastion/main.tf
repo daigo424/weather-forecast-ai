a51e00023f4cc458ed948962f3e9984352324a16
daigo424 <ishii.daigo@gmail.com>
2026-06-25 18:11:29 +0900

    feat: set bastion

---
+++ infra/terraform/modules/bastion/main.tf
@@ -0,0 +1,60 @@
+data "aws_ami" "al2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_iam_role" "bastion" {
+  name = "${var.name_prefix}-bastion"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${var.name_prefix}-bastion"
+  role = aws_iam_role.bastion.name
+}
+
+resource "aws_security_group" "bastion" {
+  name   = "${var.name_prefix}-bastion-sg"
+  vpc_id = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-bastion-sg"
+  }
+}
+
+resource "aws_instance" "bastion" {
+  ami                    = data.aws_ami.al2023.id
+  instance_type          = "t3.nano"
+  subnet_id              = var.public_subnet_id
+  iam_instance_profile   = aws_iam_instance_profile.bastion.name
+  vpc_security_group_ids = [aws_security_group.bastion.id]
+
+  tags = {
+    Name = "${var.name_prefix}-bastion"
+  }
+}
